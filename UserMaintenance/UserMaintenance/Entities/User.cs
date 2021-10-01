b1d5892c95968186dfc56a33061be48994d52fde
Sáfrán Viktor <viktor.safran@stud.uni-corvinus.hu>
2021-10-01 13:58:42 +0200

    create add button

--- UserMaintenance/UserMaintenance/Entities/User.cs
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -8,7 +8,7 @@ namespace UserMaintenance.Entities
 {
     public class User
     {
-        public Guid ID { get; set; } = Guid.NewGuid();
+        public Guid ID { get;  } = Guid.NewGuid();
         public string FirstName { get; set; }
         public string LastName { get; set; }
         
