4d1013cf7ac0afee0c7147b11af1bac2a4669ec8
Sáfrán Viktor <viktor.safran@stud.uni-corvinus.hu>
2021-10-01 14:43:18 +0200

    fájlba írás hozzáadva

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,8 +19,8 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.FullName; 
-             
+            label1.Text = Resource1.FullName;
+            button2.Text = Resource1.WriteToFile;
             button1.Text = Resource1.Add;
 
             listBox1.DataSource = users;
@@ -35,6 +36,21 @@ namespace UserMaintenance
                 
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK)
+                return;
+
+            using (var sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            
+                foreach (var u in users)
+                
+                    sw.WriteLine(string.Format("{0};{1}",u.ID,u.FullName));
+                
+            
         }
     }
 }
