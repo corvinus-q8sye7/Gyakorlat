a5f25d696494c264d91f4eb692d707d16b698202
Sáfrán Viktor <viktor.safran@stud.uni-corvinus.hu>
2021-10-29 12:05:42 +0200

    4.feladatrész kész

--- MNBSoap/MNBSoap/Form1.cs
+++ MNBSoap/MNBSoap/Form1.cs
@@ -1,4 +1,5 @@
-﻿using MNBSoap.MNBServiceReference;
+﻿using MNBSoap.Entities;
+using MNBSoap.MNBServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,10 +15,12 @@ namespace MNBSoap
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
             Consume();
+            dataGridView1.DataSource = Rates;
         }
         void Consume()
         {
