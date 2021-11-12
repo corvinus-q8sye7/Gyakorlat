355cf6e8514bd35dcf681357e95c1a8a24edf087
Sáfrán Viktor <viktor.safran@stud.uni-corvinus.hu>
2021-11-12 15:42:10 +0100

    futószalag kész

--- SantaFactory/SantaFactory/Form1.cs
+++ SantaFactory/SantaFactory/Form1.cs
@@ -1,4 +1,5 @@
 ﻿
+using SantaFactory.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -11,12 +12,51 @@ using System.Windows.Forms;
 
 namespace SantaFactory
 {
+    
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+        private BallFactory ballFactory;
+
+        public BallFactory BallFactory
+        {
+            get { return ballFactory; }
+            set { ballFactory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
-           
+            BallFactory = new BallFactory();
+
+            
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = BallFactory.CreateNew();
+            _balls.Add(ball);
+            mainPanel.Controls.Add(ball);
+            ball.Left = -ball.Width;
+            
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var lastPosition = 0;
+            foreach (var item in _balls)
+            {
+                item.MoveBall();
+                if (item.Left > lastPosition)
+                    lastPosition = item.Left;
+            }
+
+            if (lastPosition >= 1000)
+            {
+               var oldestBall = _balls[0];
+               mainPanel.Controls.Remove(oldestBall);
+               _balls.Remove(oldestBall);
+            }
         }
     }
 }
